937e5943f2b5f726e04ef9a7bfc9ae2302600bd7
Misko <misko@meta.com>
2024-08-02 11:19:10 -0700

    Balanced batch sampler+base dataset (#753)
    * Update BalancedBatchSampler to use datasets' `data_sizes` method
    Replace BalancedBatchSampler's `force_balancing` and `throw_on_error` parameters with `on_error`
    
    * Remove python 3.10 syntax
    
    * Documentation
    
    * Added set_epoch method
    
    * Format
    
    * Changed "resolved dataset" message to be a debug log to reduce log spam
    
    * clean up batchsampler and tests
    
    * base dataset class
    
    * move lin_ref to base dataset
    
    * inherit basedataset for ase dataset
    
    * filter indices prop
    
    * added create_dataset fn
    
    * yaml load fix
    
    * create dataset function instead of filtering in base
    
    * remove filtered_indices
    
    * make create_dataset and LMDBDatabase importable from datasets
    
    * create_dataset cleanup
    
    * test create_dataset
    
    * use metadata.natoms directly and add it to subset
    
    * use self.indices to handle shard
    
    * rename _data_sizes
    
    * fix Subset of metadata
    
    * minor change to metadata, added full path option
    
    * import updates
    
    * implement get_metadata for datasets; add tests for max_atoms and balanced partitioning
    
    * a[:len(a)+1] does not throw error, change to check for this
    
    * off by one fix
    
    * fixing tests
    
    * plug create_dataset into trainer
    
    * remove datasetwithsizes; fix base dataset integration; replace close_db with __del__
    
    * lint
    
    * add/fix test;
    
    * adding new notebook for using fairchem models with NEBs without CatTSunami enumeration (#764)
    
    * adding new notebook for using fairchem models with NEBs
    
    * adding md tutorials
    
    * blocking code cells that arent needed or take too long
    
    * Add extra test case for local batch size = 1
    
    * fix example
    
    * fix test case
    
    * reorg changes
    
    * remove metadata_has_sizes in favor of basedataset function metadata_hasattr
    
    * fix data_parallel typo
    
    * fix up some tests
    
    * rename get_metadata to sample_property_metadata
    
    * add slow get_metadata for ase; add tests for get_metadata (ase+lmdb); add test for make lmdb metadata sizes
    
    * add support for different backends and ddp in pytest
    
    * fix tests and balanced batch sampler
    
    * make default dataset lmdb
    
    * lint
    
    * fix tests
    
    * test with world_size=0 by default
    
    * fix tests
    
    * fix tests..
    
    * remove subsample from oc22 dataset
    
    * remove old datasets; add test for noddp
    
    * remove load balancing from docs
    
    * fix docs; add train_split_settings and test for this
    
    ---------
    
    Co-authored-by: Nima Shoghi <nimashoghi@gmail.com>
    Co-authored-by: Nima Shoghi <nimashoghi@fb.com>
    Co-authored-by: lbluque <lbluque@meta.com>
    Co-authored-by: Brandon <bmwood@meta.com>
    Co-authored-by: Brook Wander <73855115+brookwander@users.noreply.github.com>
    Co-authored-by: Muhammed Shuaibi <45150244+mshuaibii@users.noreply.github.com>
    Co-authored-by: Muhammed Shuaibi <mushuaibi@gmail.com>
    
    (cherry picked from commit 93c676185706e0692ebf0b7e2f6bc9916d07c472 [formerly d0b679c2bbfab4f320de339fe0a0ec24e5c3237a])
    
    
    Former-commit-id: 31ff3967dcd3805d3006bd2b616c9b9c13b7c299

--- src/fairchem/core/trainers/ocp_trainer.py
+++ src/fairchem/core/trainers/ocp_trainer.py
@@ -230,12 +230,6 @@ class OCPTrainer(BaseTrainer):
             if checkpoint_every == -1:
                 self.save(checkpoint_file="checkpoint.pt", training_state=True)
 
-        self.train_dataset.close_db()
-        if self.config.get("val_dataset", False):
-            self.val_dataset.close_db()
-        if self.config.get("test_dataset", False):
-            self.test_dataset.close_db()
-
     def _denorm_preds(self, target_key: str, prediction: torch.Tensor, batch: Batch):
         """Convert model output from a batch into raw prediction by denormalizing and adding references"""
         # denorm the outputs
@@ -661,7 +655,9 @@ class OCPTrainer(BaseTrainer):
                 )
                 gather_results["chunk_idx"] = np.cumsum(
                     [gather_results["chunk_idx"][i] for i in idx]
-                )[:-1]  # np.split does not need last idx, assumes n-1:end
+                )[
+                    :-1
+                ]  # np.split does not need last idx, assumes n-1:end
 
                 full_path = os.path.join(
                     self.config["cmd"]["results_dir"], "relaxed_positions.npz"
